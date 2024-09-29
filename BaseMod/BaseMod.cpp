07488ede3ad549ae71fa8fb173732177e48268fc
Paxlord <faresboukhezna@gmail.com>
2024-09-29 03:35:18 +0200

    feat: hook and update font size/format string for material list smithy

--- BaseMod/BaseMod.cpp
+++ BaseMod/BaseMod.cpp
@@ -4,13 +4,71 @@
 #include <MinHook.h>
 #include "globals.h"
 #include "config.h"
+#include "binutils.h"
+#include <cstdint>
 
 bool myCheckBox = false;
 int counter = 0;
+int font_size_x = 18;
+int font_size_y = 18;
+int trailing_spaces = 16;
+char buf_1[32];
+char buf_2[32];
+
+uint32_t hk_jmp_fontupdate_lbputiteminfo = 0x49C60A;
+uint32_t hk_ret_fontupdate_lbputiteminfo = 0x49C614;
+uint32_t hk_dat_fontupdate_lbputiteminfo = 0xEE268E0;
+void __declspec(naked) hk_fontupdate_lbputiteminfo() {
+	__asm {
+		push ecx
+		mov ecx, font_size_x
+		mov byte ptr[eax + 0x18], cl
+		mov ecx, font_size_y
+		mov byte ptr[eax + 0x19], cl
+		pop ecx
+		mov [eax+0x64], ecx
+		cmp dword ptr [hk_dat_fontupdate_lbputiteminfo], 0xA
+		jmp hk_ret_fontupdate_lbputiteminfo
+	}
+}
+
+uint32_t hk_jmp_formatstring1_lbputiteminfo = 0x49C614;
+uint32_t hk_ret_formatstring1_lbputiteminfo = 0x49C619;
+void __declspec(naked) hk_formatstring1_lbputiteminfo() {
+	__asm {
+		mov ecx, offset buf_1
+		jmp hk_ret_formatstring1_lbputiteminfo
+	}
+}
+
+uint32_t hk_jmp_formatstring2_lbputiteminfo = 0x49C61B;
+uint32_t hk_ret_formatstring2_lbputiteminfo = 0x49C620;
+void __declspec(naked) hk_formatstring2_lbputiteminfo() {
+	__asm {
+		mov ecx, offset buf_2
+		jmp hk_ret_formatstring2_lbputiteminfo
+	}
+}
+
 
 //Will run once at game start, use it to setup hooks, offset address, initialize stuff...
 void BaseMod::OnAttach() {
-	std::cout << "This is a call from test mod attach function! dll handle is : " << mhfdll_addy << std::endl;
+
+	sprintf_s(buf_1, "%%-%ds%%s%%2d(%%3d)/%%3d", trailing_spaces);
+	sprintf_s(buf_2, "%%-%ds%%s%%2d(%%3d)/%%2d", trailing_spaces);
+
+	hk_jmp_fontupdate_lbputiteminfo += mhfdll_addy;
+	hk_ret_fontupdate_lbputiteminfo += mhfdll_addy;
+	hk_dat_fontupdate_lbputiteminfo += mhfdll_addy;
+	binutils::DetourJMP((void*)hk_jmp_fontupdate_lbputiteminfo, (void*)hk_fontupdate_lbputiteminfo, 5);
+
+	hk_jmp_formatstring1_lbputiteminfo += mhfdll_addy;
+	hk_ret_formatstring1_lbputiteminfo += mhfdll_addy;
+	binutils::DetourJMP((void*)hk_jmp_formatstring1_lbputiteminfo, (void*)hk_formatstring1_lbputiteminfo, 0);
+
+	hk_jmp_formatstring2_lbputiteminfo += mhfdll_addy;
+	hk_ret_formatstring2_lbputiteminfo += mhfdll_addy;
+	binutils::DetourJMP((void*)hk_jmp_formatstring2_lbputiteminfo, (void*)hk_formatstring2_lbputiteminfo, 0);
 }
 
 //Will run once at game end, loader doesn't use it for now
@@ -26,8 +84,14 @@ void BaseMod::OnImGUIInit() {
 //Has access to the main ImGui context, can draw anything related to the mod in there
 void BaseMod::DrawModMenu() {
 	if (ImGui::CollapsingHeader(DISPLAY_NAME.c_str())) {
-		ImGui::Text("Hello From Mod %d", counter);
-		ImGui::Checkbox("Test checkbox", &myCheckBox);
+		ImGui::SliderInt("Font Size X", &font_size_x, 1, 48);
+		ImGui::SliderInt("Font Size Y", &font_size_y, 1, 48);
+		if (ImGui::SliderInt("Material name spaces (default 16)", &trailing_spaces, 1, 48)) {
+			sprintf_s(buf_1, "%%-%ds%%s%%2d(%%3d)/%%3d", trailing_spaces);
+			sprintf_s(buf_2, "%%-%ds%%s%%2d(%%3d)/%%2d", trailing_spaces);
+		}
+		//ImGui::InputText("Format string 1", buf_1, 32);
+		//ImGui::InputText("Format string 2", buf_2, 32);
 	}
 }
 
--- BaseMod/config.h
+++ BaseMod/config.h
@@ -2,8 +2,8 @@
 #include <string>
 
 //MOD CONFIG
-inline const std::string NAME = "awesomeMod"; //Name of the mod, used for the required/allowed checks and to check if loaded mod is unique
-inline const std::string DISPLAY_NAME = "My Test Mod"; //Display name in game, used mainly for the imgui context
+inline const std::string NAME = "font-tweaker"; //Name of the mod, used for the required/allowed checks and to check if loaded mod is unique
+inline const std::string DISPLAY_NAME = "Font Tweaker"; //Display name in game, used mainly for the imgui context
 inline const std::string VERSION = "0.0.1"; //Version of the mod, used to check in require/allowed list
 inline const int REQUIRED_VERSION = 2; //Required loader version for this mod
 inline const bool HGE_ONLY = false; //Will only run if the loader blocks Low-Grade edition from launching 
